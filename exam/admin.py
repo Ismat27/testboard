153debe161623b4df013aff739511ca4f5150ede
ismail adegbite <adegbiteismail27@gmail.com>
2023-08-27 17:34:35 +0100

    register table

--- exam/admin.py
+++ exam/admin.py
@@ -1,6 +1,9 @@
 from django.contrib import admin
-from .models import Assessment,  AssessmentResult
+from .models import Assessment,  AssessmentResult, AssessmentLogin, AssessmentToken, Result
 
 # Register your models here.
 admin.site.register(Assessment)
 admin.site.register(AssessmentResult)
+admin.site.register(AssessmentLogin)
+admin.site.register(AssessmentToken)
+admin.site.register(Result)
